4e5a8c530da79ba44c3703eed8894b8b1aa5664b
Laura García de Marina Vázquez <lauragarciademarina@gmail.com>
2026-06-02 22:04:17 +0200

    remove base path

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  basePath: "/15MinuteCity",
-  assetPrefix: "/15MinuteCity",
   trailingSlash: false,
-  redirects: async () => [
-    {
-      source: "/",
-      destination: "/15MinuteCity",
-      permanent: true,
-    },
-  ],
 };
 
 export default nextConfig;
